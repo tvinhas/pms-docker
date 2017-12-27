1c0fa424aa1daee363f441aa9fbf773fe6e328b3
Thiago Vinhas <tvinhas@users.noreply.github.com>
2017-12-27 12:37:30 -0500

    Create remux.sh

---
+++ remux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $# -lt 1 ]
+then
+    echo "Usage: `basename $0` <filename> (must be a mkv file)"
+        exit 1
+fi
+
+#vars
+file=$1
+outmkv="${file%.*}.mkv"
+logfile="/config/tmp/remux.log"
+
+if ffmpeg -fflags +genpts -i "$file" -c:v copy -c:a copy -preset superfast -sn -movflags faststart "$outmkv"  &>> $logfile
+then
+  echo "Success, removing $file" &>> $logfile
+  rm -fv "$file"
+  chown plex: "$outmkv"
+else
+  echo "Failure, exit status $?" &>> $logfile
+  rm -fv "$outmkv"
+fi
